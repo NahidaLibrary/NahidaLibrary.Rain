a7f9969ab9d04606879b69a83b7b5dbf042f5719
luoshen19 <lkzc19@gmail.com>
2024-01-13 01:42:16 +0800

    feat: 大致布局

---
+++ src/components/NFooter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>n footer</div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/NHeader.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+
+const router = useRouter()
+const menuList = reactive([
+  { name: '关于', path: 'about' },
+  { name: '音乐', path: 'music' },
+  { name: '登录', path: 'login' }
+])
+// 当前选择菜单
+const activeMenu = ref('index')
+// 切换导航跳转
+const selectMenu = (item: string) => {
+  if (item === 'login') return
+  router.push({ path: '/' + item })
+  activeMenu.value = item
+}
+</script>
+
+<template>
+  <div class="header-container">
+    <router-link to="/" class="logo">纳西妲图书馆</router-link>
+    <el-space :size="30" spacer="">
+      <div
+        v-for="item in menuList"
+        :key="item.path"
+        class="nav"
+        :class="{ 'active': activeMenu === item.path }"
+        @click="selectMenu(item.path)"
+      >
+        <span>{{ item.name }}</span>
+      </div>
+    </el-space>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.header-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .logo {
+    font-size: 2rem;
+    font-weight: bold;
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  .nav {
+    font-size: 1.3rem;
+    font-weight: 700;
+
+    &.active {
+      color: pink;
+    }
+    span:hover {
+      color: pink;
+    }
+  }
+}
+</style>
